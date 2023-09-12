4a3dcc2287776df2af3c677c6dc88ea446fc2e84
Marcos Vinicius da Silva <PET0154085@aluno.firjansenaisesi.com.br>
2023-09-11 21:43:10 -0300

    exercicios

---
+++ exercicios.js
@@ -0,0 +1,49 @@
+function exercicios1(){
+    alert("exercicios1")
+    let numero1;
+    let numero2;
+    numero1=prompt("digie um numero")
+    numero2=prompt("digite outro numero")
+    numero1=parseInt(numero1)
+    numero2=parseInt(numero2)
+    soma=numero1+numero2
+    alert("A soma dos numeros é = " +soma)
+}
+function exercicios2(){
+    alert("exercicios2" )
+let nome;
+let sobrenome;
+nome=prompt("digite o primeiro nome" )
+sobremome=prompt("digit o segundo nome" )
+alert("óla, " +nome+" "+sobrenome )
+}
+function exercicios3(){
+    let valorEmreal 
+    let multiplicadordolar = 4.93;
+    let cotacao;
+
+    valorEmreal =prompt ("Qual valor deseja converter?" )
+    
+    multiplicadordolar = 4.95;
+    cotacao = valorEmreal/multiplicadordolar;
+    alert(cotacao );
+}
+function exercicios4(){
+let numero = prompt ( "digite um valor" )
+numero = parseInt (numero )
+let sucessor 
+let antecessor
+sucessor = numero++
+antecessor = numero--
+alert (`o antecessor de ${numero} é ${antecessor}e o sucessor é ${sucessor}` )
+}
+function exercicios5(){
+let valorMetro = prompt("Digite a distancia km" )
+valorMetro
+}
+
+
+
+
+
+exercicios4()
